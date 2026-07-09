98bc8f1af0011cbcd6dfe2540ef0282ebd576a26
Macisso <macedopedrocunha@gmail.com>
2026-07-09 18:16:00 -0300

    Update main.c

--- Mario Kart/main.c
+++ Mario Kart/main.c
@@ -2,48 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
+#include <time.h>
 #include "function.h"
 
 int main() {
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(NULL)); // Inicializa o gerador de números aleatórios para o sorteio de itens
+
+    
+    node* lista_pilotos = NULL;
+    node_corrida* fila_corridas = NULL;
+    itens estoque_central;
+    inicializar_estoque(&estoque_central);
+
     int resp_menu;
     do {
         print_menu();
+        printf("Escolha uma opção do Menu Principal: ");
         scanf("%d", &resp_menu);
         getchar();
+
         switch (resp_menu) {
             case 0:
                 printf("\nVai e não volta mais >:(\n");
                 break;
             case 1:
-                printf("\nEm andamento! :/\n\n");
-                // corrida()
+                cadastrar_corrida(&fila_corridas);
                 break;
             case 2:
-                menu_oficina();
+                oficina();
                 break;
             case 3:
-                printf("\nSe a opção correr está em andamento, o que tu esperava do ranking?????\n");
-                // ranking()
+                menu_ranking();
                 break;
             case 4:
-                printf("\nVai ser curioso na casa do caralho\n");
+                item_dist(lista_pilotos, &estoque_central);
                 break;
             case 5:
-                printf("\nOlha aqui meu personagem Ó 8======D\n");
-                // criar()
+                criar_personagem(&lista_pilotos);
                 break;
             case 6:
-                printf("\nVai ser curioso na casa do caralho 2\n");
+                procurar_personagem(lista_pilotos);
                 break;
             case 7:
-                printf("\nSe tu foi banido, ta fazendo o que aqui seu merda!\n");
+                listar_personagens_banidos(lista_pilotos);
                 break;
             case 8:
-                printf("\nQUER ACESSAR OS ITENS PQ????\n");
+                menu_itens_gerenciar(&estoque_central);
                 break;
             default:
                 printf("\nRESPOSTA ERRADA!\n");
                 break;
         }
     } while (resp_menu != 0);
+
+    while(lista_pilotos != NULL){
+        node* aux = lista_pilotos;
+        lista_pilotos = lista_pilotos->prox;
+        free(aux);
+    }
+    while(fila_corridas != NULL){
+        node_corrida* aux = fila_corridas;
+        fila_corridas = fila_corridas->prox;
+        free(aux);
+    }
+
+    return 0;
 }
