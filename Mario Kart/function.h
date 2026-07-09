6b6c346dfb33b054084aed6ba72dacdca1b6ce6e
Macisso <macedopedrocunha@gmail.com>
2026-07-09 18:15:35 -0300

    Refactor data structures and update function prototypes

--- Mario Kart/function.h
+++ Mario Kart/function.h
@@ -1,40 +1,62 @@
 #ifndef function_h
 #define function_h
 
-typedef struct DADOS personagem;
-typedef struct NODE node;
-typedef struct ITEM itens;
-typedef struct PISTA corrida;
+// 1. Definição das Estruturas de Dados Reais
+typedef struct DADOS {
+    char piloto[30];
+    int categ, cont_trofeu, st_pil;
+    int v_kart, ac, ctrl, resist, st_kart;
+} personagem;
+
+typedef struct NODE {
+    personagem dado;
+    struct NODE* prox;
+} node;
+
+typedef struct ITEM {
+    int tipo; 
+    int raridade; 
+    int poder;
+    int quantidade[8];
+} itens;
+
+typedef struct PISTA {
+    int pista; 
+    int num_voltas;
+    int dificuldade; 
+    int status; 
+    int prioridade;
+} corrida;
 
 typedef struct NODE_CORRIDA {
     corrida dados_corrida;
     struct NODE_CORRIDA* prox; 
-}node_corrida;
+} node_corrida;
 
 void print_menu();
 
 // MÓDULO 1 - CORRIDA
-void cadastrar_corrida(node_corrida** inicio);
-corrida iniciar_corrida(node_corrida** inicio_fila, int*sucesso);
-void visualizar_corridas(node_corrida* inicio);
+void cadastrar_corrida(node_corrida** inicio_fila);
 
-//MÓDULO 2 – REGISTRO DE PILOTOS
+// MÓDULO 2 – REGISTRO DE PILOTOS
 void criar_personagem(node** inicio);
 void procurar_personagem(node* inicio);
 void listar_personagens_banidos(node* inicio);
 
-//MÓDULO 3 – ITENS
+// MÓDULO 3 – ITENS
 void menu_itens();
-void item_dist();
+void inicializar_estoque(itens* estoque);
+void exibir_estoque(itens* estoque);
+void repor_estoque(itens* estoque);
+void menu_itens_gerenciar(itens* estoque);
+void item_dist(node* lista_pilotos, itens* estoque); 
 
-//MÓDULO 4 – HISTÓRICO
+// MÓDULO 4 – HISTÓRICO
 void ordenar(node* cabeca);
 void menu_ranking();
-void historico();
 
-//MÓDULO 5 – OFICINA
+// MÓDULO 5 – OFICINA
 void menu_oficina();
-
-//MÓDULO 6 – CAMPEONATO E PONTUAÇÃO
+void oficina();
 
 #endif
