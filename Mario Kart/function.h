7ec8253b7c1a99fa1c0eda8f7b0dcae423bcebc8
Jkuesh <williamminecraftlbjo@gmail.com>
2026-07-11 20:59:33 -0300

    Update function.h

--- Mario Kart/function.h
+++ Mario Kart/function.h
@@ -1,67 +1,14 @@
 #ifndef function_h
 #define function_h
 
-// ==========================================
-// ESTIDADE 1: PILOTO & KART (Lista Encadeada)
-// ==========================================
-typedef struct DADOS {
-    char piloto[30];
-    int categ;          // 1-Leve, 2-Médio, 3-Pesado
-    int cont_trofeu;    // Pontuação acumulada no campeonato
-    int st_pil;         // 0-Ativo, 1-Suspenso
-    int v_kart, ac, ctrl, resist; // Atributos do Kart
-    int st_kart;        // 0-Operacional, 1-Danificado, 2-Destruído
-} personagem;
+typedef struct DADOS personagem;
+typedef struct NODE node;
+typedef struct corrida;
+typedef struct NODE_CORRIDA node_corrida;
+typedef struct ITEM itens;
+typedef struct NODE_HISTORICO node_historico;
+typedef struct NODE_OFICINA node_oficina;
 
-typedef struct NODE {
-    personagem dado;
-    struct NODE* prox;
-} node;
-
-// ==========================================
-// ENTIDADE 2: CENTRAL DE CORRIDAS (Fila FIFO)
-// ==========================================
-typedef struct PISTA {
-    int pista;         // ID da pista (0 a 7)
-    int num_voltas;
-    int dificuldade;   // Nível de Perigo (1 a 100)
-    int status;        // 0-Aguardando, 1-Em andamento, 2-Finalizada
-} corrida;
-
-typedef struct NODE_CORRIDA {
-    corrida dados_corrida;
-    struct NODE_CORRIDA* prox; 
-} node_corrida;
-
-// ==========================================
-// ENTIDADE 3: ARSENAL DE ITENS (Estrutura Global)
-// ==========================================
-typedef struct ITEM {
-    int quantidade[8]; // Estoque das 8 variedades de itens
-} itens;
-
-// ==========================================
-// ENTIDADE 4: HISTÓRICO (Pilha LIFO - Nova!)
-// ==========================================
-typedef struct NODE_HISTORICO {
-    char pista_nome[50];
-    char vencedor_nome[30];
-    int total_pilotos;
-    struct NODE_HISTORICO* topo; // Aponta para a corrida anterior na pilha
-} node_historico;
-
-// ==========================================
-// ENTIDADE 5: OFICINA (Fila de Prioridade)
-// ==========================================
-typedef struct NODE_OFICINA {
-    char piloto_nome[30];
-    int status_kart; // 1-Danificado, 2-Destruído
-    struct NODE_OFICINA* prox;
-} node_oficina;
-
-// ==========================================
-//                  MODULOS
-// ==========================================
 void print_menu();
 
 // MÓDULO 1 - CENTRAL DE CORRIDAS
