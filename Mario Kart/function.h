5ee1276f39de3ecc9b7f9dcbf87c9ea3340d95d6
Jkuesh <williamminecraftlbjo@gmail.com>
2026-07-13 02:31:18 -0300

    Update function.h

--- Mario Kart/function.h
+++ Mario Kart/function.h
@@ -3,7 +3,7 @@
 
 typedef struct DADOS personagem;
 typedef struct NODE node;
-typedef struct PISTA corrida; // Corrigido de 'struct corrida'
+typedef struct PISTA corrida;
 typedef struct NODE_CORRIDA node_corrida;
 typedef struct ITEM itens;
 typedef struct NODE_HISTORICO node_historico;
@@ -12,13 +12,13 @@ typedef struct OFICINA oficina;
 void print_menu();
 
 // MÓDULO 1 - CENTRAL DE CORRIDAS
-void cadastrar_corrida(node_corrida** inicio_fila); // Removido '**fim' que não existe no .c!!!
-void realizar_corrida(node_corrida** inicio_fila, node* lista_pilotos, itens* estoque, int* cont_acidentes); // Removidos parâmetros extras
+void cadastrar_corrida(node_corrida** inicio_fila);
+void realizar_corrida(node_corrida** inicio_fila, node* lista_pilotos, itens* estoque, int* cont_acidentes);
 
 // MÓDULO 2 – REGISTRO DE PILOTOS
 void criar_personagem(node** inicio);
-void procurar_personagem(node** inicio); // Ajustado para node** igual ao .c!!!
-void pers_banidos(); // Nome corrigido para bater com o .c
+void procurar_personagem(node** inicio);
+void pers_banidos();
 void ordenar(node* cabeca);
 void ordenar_performance(node* lista_pilotos);
 
@@ -31,7 +31,7 @@ void menu_itens_gerenciar(node* lista_pilotos, itens* estoque);
 void item_dist(node* lista_pilotos, itens* estoque);
 
 // MÓDULO 4 – HISTÓRICO (PILHA)
-void exibir_historico(node* lista_pilotos); // Ajustado para bater com o .c!!!
+void exibir_historico(node* lista_pilotos);
 
 // MÓDULO 5 – OFICINA
 void menu_oficina(oficina** fila_oficina, node* lista_pilotos);
@@ -39,9 +39,16 @@ void registrar_manutencao(oficina** topo_oficina, char* nome, int status);
 void iniciar_reparo(oficina** topo_oficina, node* lista_pilotos);
 void exibir_fila_oficina(oficina* topo_oficina);
 
-//MÓDULO 6 - SISTEMA E INFORMAÇÕES GERAIS
+// MÓDULO 6 - SISTEMA E INFORMAÇÕES GERAIS
 void atualizar_pontuacao_corrida(node* lista_pilotos, int posicao, node* piloto);
 void exibir_ranking_campeonato(node* lista_pilotos);
 void determinar_campeao_temporada(node* lista_pilotos);
+
+// SISTEMA DE ARMAZENAMENTO
+
+void salvar(node* lista_pilotos);
+void carregar(node** lista_pilotos, oficina** fila_oficina);
+void salvar_historico(node_historico* pilha_historico);
+void carregar_historico(node_historico** pilha_historico);
 
 #endif
