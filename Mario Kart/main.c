56ea7e787f38deca9f4a7f610de013d9138da48b
Macisso <macedopedrocunha@gmail.com>
2026-07-12 22:24:29 -0300

    Update main.c

--- Mario Kart/main.c
+++ Mario Kart/main.c
@@ -14,12 +14,12 @@ int main() {
     node_corrida* fila_corrida_inicio = NULL; // 2. Fila (Início)
     node_corrida* fila_corrida_fim = NULL;    // 2. Fila (Fim)
     node_historico* pilha_historico = NULL;// 3. Pilha de Histórico
-    node_oficina* fila_oficina = NULL;     // 4. Fila de Prioridade da Oficina
+    oficina* fila_oficina = NULL;     // 4. Fila de Prioridade da Oficina
 
     itens estoque_central;
     inicializar_estoque(&estoque_central);
 
-    int resp_menu;
+    int resp_menu, cont_acidentes = 0;
     do {
         print_menu();
         printf("Escolha uma opção: ");
@@ -30,66 +30,73 @@ int main() {
         getchar(); // Limpa o buffer
 
         switch (resp_menu) {
-            case 1: {
-                int sub;
-                printf("\n--- MÓDULO 1: CORRIDAS ---\n");
-                printf("1 - Agendar Nova Corrida\n");
-                printf("2 - Iniciar Próxima Corrida da Fila\n");
-                printf("Escolha: ");
-                scanf("%d", &sub);
-                getchar();
-                if (sub == 1) cadastrar_corrida(&fila_corrida_inicio, &fila_corrida_fim);
-                else if (sub == 2) realizar_corrida(&fila_corrida_inicio, &fila_corrida_fim, lista_pilotos, &pilha_historico);
-                else printf("Opção inválida.\n");
-                break;
+        case 1: {
+            int sub;
+            printf("\n--- MÓDULO 1: CORRIDAS ---\n");
+            printf("1 - Agendar Nova Corrida\n");
+            printf("2 - Iniciar Próxima Corrida da Fila\n");
+            printf("Escolha: ");
+            scanf("%d", &sub);
+            getchar();
+            if (sub == 1) {
+                cadastrar_corrida(&fila_corrida_inicio);
+            } else if (sub == 2) {
+                realizar_corrida(&fila_corrida_inicio, lista_pilotos, &estoque_central, &cont_acidentes);
+            } else {
+                printf("Opção inválida.\n");
             }
-            case 2:
-                oficina(&fila_oficina, lista_pilotos);
-                break;
-            case 3:
-                exibir_historico(pilha_historico);
-                break;
-            case 4:
-                item_dist(lista_pilotos, &estoque_central);
-                break;
-            case 5:
-                criar_personagem(&lista_pilotos);
-                break;
-            case 6:
-                procurar_personagem(lista_pilotos);
-                break;
-            case 7:
-                listar_personagens_banidos(lista_pilotos);
-                break;
-            case 8:
-                menu_itens_gerenciar(&estoque_central);
-                break;
-            case 0:
-                printf("\nEncerrando o sistema oficial da FMKR. Até mais!\n");
-                break;
-            default:
-                printf("\nOpção Inválida!\n");
-                break;
+            break;
+        }
+        case 2:
+            menu_oficina(&fila_oficina, lista_pilotos);
+            break;
+        case 3:
+            exibir_ranking_campeonato(lista_pilotos);
+            break;
+        case 4:
+            exibir_historico(lista_pilotos);
+            break;
+        case 5:
+            criar_personagem(&lista_pilotos);
+            break;
+        case 6:
+            procurar_personagem(&lista_pilotos);
+            break;
+        case 7:
+            pers_banidos();
+            break;
+        case 8:
+            menu_itens_gerenciar(lista_pilotos, &estoque_central);
+            break;
+        case 0:
+            determinar_campeao_temporada(lista_pilotos);
+            printf("Precione ENTER para sair... ");
+            getchar();
+            printf("\nEncerrando o sistema oficial da FMKR. Até mais!\n");
+            break;
+        default:
+            printf("\nOpção Inválida!\n");
+            break;
         }
     } while (resp_menu != 0);
 
     // Desalocação de memória ao sair
-    while(lista_pilotos != NULL) { 
-        node* aux = lista_pilotos; lista_pilotos = lista_pilotos->prox; 
-        free(aux); 
-    }
-    while(fila_corrida_inicio != NULL) { 
-        node_corrida* aux = fila_corrida_inicio; fila_corrida_inicio = fila_corrida_inicio->prox;
-         free(aux); 
-    }
-    while(pilha_historico != NULL) { 
-        node_historico* aux = pilha_historico; pilha_historico = pilha_historico->topo; 
-        free(aux); 
-    }
-    while(fila_oficina != NULL) { 
-        node_oficina* aux = fila_oficina; fila_oficina = fila_oficina->prox; 
+    while (lista_pilotos != NULL) {
+        node* aux = lista_pilotos; lista_pilotos = lista_pilotos->prox;
         free(aux);
-     }
+    }
+    while (fila_corrida_inicio != NULL) {
+        node_corrida* aux = fila_corrida_inicio; fila_corrida_inicio = fila_corrida_inicio->prox;
+        free(aux);
+    }
+    while (pilha_historico != NULL) {
+        node_historico* aux = pilha_historico; pilha_historico = pilha_historico->topo;
+        free(aux);
+    }
+    while (fila_oficina != NULL) {
+        oficina* aux = fila_oficina; fila_oficina = fila_oficina->prox;
+        free(aux);
+    }
 
     return 0;
 }
